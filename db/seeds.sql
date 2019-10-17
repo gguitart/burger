37592c7080bbd3aa4815929f18579d3e590d4dea
GG Guitart <gertaguitart2019@u.northwestern.edu>
2019-10-17 07:50:02 -0500

    first upload

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Burger');
